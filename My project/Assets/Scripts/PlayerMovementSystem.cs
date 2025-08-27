9d29c370b29649825b03fc1189a1adbc2e6ebc5f
Nikita Glubokov <ITVenomancer@gmail.com>
2025-08-27 17:59:13 +0800

    Fixed movement; wobble animation; sprite faces direction

--- My project/Assets/Scripts/PlayerMovementSystem.cs
+++ My project/Assets/Scripts/PlayerMovementSystem.cs
@@ -4,35 +4,50 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 5f;
 
+    private Animator _playerAnimator;
+
     private Rigidbody2D _rigidbody;
     private Vector2 _movementVector;
 
-    void Start()
+    private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _playerAnimator = GetComponent<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
-        if (moveX != 0)
+        _movementVector = new Vector2(moveX, moveY).normalized;
+
+        if (moveX != 0 || moveY != 0)
         {
-            _movementVector = new Vector2(moveX, 0);
-        }
-        else if (moveY != 0)
-        {
-            _movementVector = new Vector2(0, moveY);
+            _playerAnimator.CrossFade("PlayerShakeMovement", 0, 0);
         }
         else
         {
             _movementVector = Vector2.zero;
+            _playerAnimator.CrossFade("PlayerShakeIdle", 0, 0);
         }
+
+        RotatePlayerSprite(moveX, moveY);
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        _rigidbody.linearVelocity = _movementVector.normalized * _moveSpeed;
+        _rigidbody.linearVelocity = _movementVector * _moveSpeed;
+    }
+    public void RotatePlayerSprite(float vectorX, float vectorY)
+    {
+        if(vectorX < 0 || vectorY < 0)
+        {
+            gameObject.transform.localScale = new Vector2(-1, 1);
+        }
+        else
+        {
+            gameObject.transform.localScale = new Vector2(1, 1);
+        }
     }
 }
